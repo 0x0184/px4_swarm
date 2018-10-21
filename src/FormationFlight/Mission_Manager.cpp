4332865fe91dc83d086d94b26f47eca9fc590206
rapsealk <piono623@naver.com>
2018-10-21 21:24:53 +0900

    SYSTEM_ID=2 드론을 리더로 설정 (mavros1)

--- src/FormationFlight/Mission_Manager.cpp
+++ src/FormationFlight/Mission_Manager.cpp
@@ -46,108 +46,18 @@ int main(int argc, char** argv)
   ros::NodeHandle n;
   sensor_msgs::NavSatFix tmp;
 
-
-  // 2017-09-07
-
-  tmp.latitude = 37.6008854;
-  tmp.longitude = 126.866451;
-  tmp.altitude = 5;
-  InputMission(tmp, 0);
-
-  tmp.latitude = 37.600807;
-  tmp.longitude = 126.8667029;
-  tmp.altitude = 5;
-  InputMission(tmp, 1);
-
-  tmp.latitude = 37.6005007;
-  tmp.longitude = 126.8666028;
-  tmp.altitude = 5;
-  InputMission(tmp, 2);
-
-  tmp.latitude = 37.6005007;
-  tmp.longitude = 126.8666028;
-  tmp.altitude = 3;
-  InputMission(tmp, 3);
-
-  tmp.latitude = 37.6005435;
-  tmp.longitude = 126.8664685;
-  tmp.altitude = 5;
-  InputMission(tmp, 4);
-
-  tmp.latitude = 37.6006283;
-  tmp.longitude = 126.8664249;
-  tmp.altitude = 7;
-  InputMission(tmp, 5);
-
-  tmp.latitude = 37.6007109;
-  tmp.longitude = 126.8663684;
-  tmp.altitude = 5;
-  InputMission(tmp, 6);
-
-  tmp.latitude = 37.6006794;
-  tmp.longitude = 126.8662114;
-  tmp.altitude = 5;
-  InputMission(tmp, 7);
-
-  tmp.latitude = 37.6007491;
-  tmp.longitude = 126.8662097;
-  tmp.altitude = 5;
-  InputMission(tmp, 8);
-
-  tmp.latitude = 37.6007818;
-  tmp.longitude = 126.8662665;
-  tmp.altitude = 5;
-  InputMission(tmp, 9);
-
-  tmp.latitude = 37.6009635;
-  tmp.longitude = 126.8662035;
-  tmp.altitude = 3;
-  InputMission(tmp, 10);
-
-
-////////////////////  본관////////////////
-/*  tmp.latitude=37.5995457;
-  tmp.longitude=126.863386;
-  tmp.altitude=3;
-  InputMission(tmp,0);
-
-
-
-
-
-  tmp.latitude=37.5996187;
-  tmp.longitude=126.8632602;
-  tmp.altitude=3;
-  InputMission(tmp,1);
-
-
-
-
-  tmp.latitude=37.599537;
-  tmp.longitude=126.8631766;
-  tmp.altitude=3;
-  InputMission(tmp,2);
-
-
-
-
-  tmp.latitude=37.5994378;
-  tmp.longitude=126.8633271;
-  tmp.altitude=3;
-  InputMission(tmp,3);
-
-
-  tmp.latitude=37.5993906;
-  tmp.longitude=126.8635055;
-  tmp.altitude=3;
-  InputMission(tmp,4);
-
-
-  tmp.latitude=37.5995247;
-  tmp.longitude=126.8634441;
-  tmp.altitude=3;
-  InputMission(tmp,5);*/
-
+  double latitudes[MissionSize] = {
+      // TODO
+  };
+  double longitudes[MissionSize] = {
+      // TODO
+  };
+  for (int i = 0; i < MissionSize; i++) {
+      tmp.latitude = latitudes[i];
+      tmp.longitude = longitudes[i];
+      tmp.altitude = 5;
+      InputMission(tmp, i);
+  }
 
   ros::Subscriber arr_sub=n.subscribe("/arrived1", 10, ReceiveMissionReceived);
 //  mission_pub = n.advertise<sensor_msgs::NavSatFix>("/manual_Mission",10);
