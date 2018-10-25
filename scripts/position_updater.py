d3b812e70e2550159a4944342a8786648b0357c1
rapsealk <piono623@naver.com>
2018-10-25 16:34:58 +0900

    Update topic "/mavros/global_position/global" on remote database.

--- scripts/position_updater.py
+++ scripts/position_updater.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import rospy
 from sensor_msgs.msg import NavSatFix
 import pymysql.cursors
@@ -10,12 +10,14 @@ DATABASE_PASS = 'PASSWORD_OF_ROOT'
 DATABASE_NAME = 'uavdb'
 
 # Connect to the database
+"""
 conn = pymysql.connect(host=DATABASE_HOST,
                         user=DATABASE_USR,
                         password=DATABASE_PASS,
                         db=DATABASE_NAME,
                         charset='utf8mb4',
                         cursorclass=pymysql.cursors.DictCursor)
+"""
 """
 def callback(message):
     global conn
@@ -27,10 +29,11 @@ def callback(message):
 """
 def callback_with_system_id(message, system_id='1'):
     global conn
-    rospy.loginfo(rospy.get_caller_id() + " received: (lat: {0}, lon: {1})".format(message.latitude, message.longitude))
-    query = 'INSERT INTO realtime(uav_id, latitude, longitude, timestamp) VALUE(%s, %s, %s, %s)'
-    conn.cursor().execute(query, (system_id, float(message.latitude), float(message.longitude), datetime.datetime.now()))
+    rospy.loginfo(rospy.get_caller_id() + " received: (lat: {0}, lon: {1})".format(message.latitude, message.longitude)
+    query = 'UPDATE realtime SET Lattitude = %s, Longitude = %s, ReceiveDate = %s WHERE UAV_ID = %s'
+    conn.cursor().execute(query, (message.latitude, message.longitude, datetime.datetime.now(), system_id))
     conn.commit()
+    print('id: {}, lat={}, lng={}'.format(system_id, message.latitude, message.longitude))
 
 def callback_generator(uav_id):
     return lambda message: callback_with_system_id(message, system_id=uav_id)
@@ -38,9 +41,14 @@ def callback_generator(uav_id):
 
 def listener():
     rospy.init_node('position_updater', anonymous=True)
-    rospy.Subscriber('mavros1/global_position/global', NavSatFix, callback_generator(uav_id='1'))
-    rospy.Subscriber('mavros2/global_position/global', NavSatFix, callback_generator(uav_id='2'))
-    rospy.Subscriber('mavros3/global_position/global', NavSatFix, callback_generator(uav_id='3'))
+    # leader id 2
+    # follower id 3 4
+
+    # Latitude Longitude GPSAltitude PressureAltitude ReceiveDate roll pitch raw
+
+    rospy.Subscriber('mavros1/global_position/global', NavSatFix, callback_generator(uav_id='2'))
+    rospy.Subscriber('mavros2/global_position/global', NavSatFix, callback_generator(uav_id='3'))
+    rospy.Subscriber('mavros3/global_position/global', NavSatFix, callback_generator(uav_id='4'))
     rospy.spin()
 
 
